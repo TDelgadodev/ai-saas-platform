01041845785a93c9f567ff590008ce5b38ac2a88
Thiago <thiagodelgado.dev@gmail.com>
2024-02-29 09:42:53 -0300

    Add image form in process

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,33 +1,37 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null; 
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = ( global as any ).mongoose;
+let cached: MongooseConnection = (global as any).mongoose
 
-if (!cached) {
-    cached = (global as any).mongoose = {
-        conn: null, promise: null
-    }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+  console.log("Connecting to the database...");
+  if(cached.conn) {
+    console.log("Database connection already exists.");
+    return cached.conn;
+  }
 
-    if (!MONGODB_URL) {
-        throw new Error('MONGODB_URL is not defined!')
-    };
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-        dbName: 'imaginify',
-        bufferCommands: false,
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
     })
 
-    cached.conn = await cached.promise
+  cached.conn = await cached.promise;
+  console.log("Database connection established successfully.");
 
-    return cached.conn;
+  return cached.conn;
 }
