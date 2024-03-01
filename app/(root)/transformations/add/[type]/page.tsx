c1a87527eb43adbe4e9004797c4f867071d9a9a8
Thiago <thiagodelgado.dev@gmail.com>
2024-03-01 18:18:29 -0300

    Add Image Form, React Hook Form + Zod validation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,29 +5,31 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = async ({ params: { type }}: SearchParamProps ) => {
-  const { userId } = auth()
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in')
-  
-  const user = await getUserById(userId);  
-  
-  if(!user) return Error('User not defined')
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  if (!user) return Error("User not defined");
   return (
     <>
-      <Header 
-        title={transformation.title}
-        subTitle={transformation.subTitle} 
-      />
-      <TransformationForm 
-         action="Add"
-         userId={user.id} 
-         type={transformation.type as TransformationTypeKey}
-         creditBalance={user.creditBalance}
-      />
+      <Header title={transformation.title} subTitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user.id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
-  )
+  );
 };
 
 export default AddTransformationTypePage;
