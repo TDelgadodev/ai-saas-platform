dca58c8133464322d62465ac809f47fd10957d73
Thiago <thiagodelgado.dev@gmail.com>
2024-03-04 10:41:53 -0300

    Cloudinary media uploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
+
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -27,6 +29,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
